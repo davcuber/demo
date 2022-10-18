34d3bc267329bbddf43f3db4f47efecebbeaca7a
Cuberes Moreno <david.cuberes@capgemini.com>
2022-10-18 13:41:49 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/cat")
+    public String indexCat() {
+        return "Salutacions des de Spring Boot!";
+    }
+}
